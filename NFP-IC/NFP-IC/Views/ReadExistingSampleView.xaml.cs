b2c26596c49a1491539e071d0131590bd258168e
Philip <jones.pmj@gmail.com>
2016-03-21 23:53:53 -0600

    Read tag UID in read existing tag

--- NFP-IC/NFP-IC/Views/ReadExistingSampleView.xaml.cs
+++ NFP-IC/NFP-IC/Views/ReadExistingSampleView.xaml.cs
@@ -15,6 +15,13 @@ using Windows.Services.Maps;
 using Newtonsoft.Json;
 using System.Net;
 using Windows.Web.Http;
+using Windows.Networking.Proximity;
+
+using System.IO;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+
 
 
 namespace NFP_IC.Views
@@ -40,10 +47,199 @@ namespace NFP_IC.Views
         private CancellationTokenSource _cts = null;
         private String Stringlat, Stringlng, addressString;
 
+        // The list of records 
+        private List<NdefRecord> recordList;
+
+        // The subscription ID from ProximityDevice
+        private long subscriptionId;
+
+        // ProximityDevice instance
+        private ProximityDevice device;
+
+        // The log content which will be filled with the details of each
+        // detected NFC tag
+        private string logText;
+
+        //private readonly CoreDispatcher _dispatcher;
+
         public ReadExistingSampleView()
         {
             this.InitializeComponent();
+            recordList = new List<NdefRecord>();
+            device = ProximityDevice.GetDefault();
+            device.DeviceArrived += DeviceArrived;
+            device.DeviceDeparted += DeviceDeparted;
+            SubscribeForMessage();
         }
+
+
+
+
+         private async void DeviceDeparted(ProximityDevice sender)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,() =>
+            {
+                logText = logText;// + "\nLost at " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second + "\n";
+                AppText.Text = "\n" + logText; 
+            });
+        }
+
+        /// <summary>
+        /// Gets called when a NFC device is detected.
+        /// </summary>
+        /// <param name="sender">ProximityDevice instance</param>
+        private async void DeviceArrived(ProximityDevice sender)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                //logText = "\nDetected at " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+                logText = "Boo";
+            });
+        }
+
+        /// <summary>
+        /// Gets called when a message is received. Updates the UI by adding
+        /// the details into the log and scrolling the log if necessary.
+        /// Note that subscription for messages needs to be redone.
+        /// </summary>
+        /// <param name="sender">ProximityDevice instance</param>
+        /// <param name="message">A message to be handled.</param>
+        private async void MessageReceived(ProximityDevice sender, ProximityMessage message)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            { 
+                if (device != null)
+                {
+                    device.StopSubscribingForMessage(subscriptionId);
+                }
+
+                logText = ParseNDEF(message);
+                //ScrollViewer.UpdateLayout();
+                //ScrollViewer.ScrollToVerticalOffset(0); 
+                SubscribeForMessage();
+            });            
+        }
+
+        /// <summary>
+        /// Parses the details from the given message. The output is a string
+        /// that can be appended into the log.
+        /// </summary>
+        /// <param name="message">The message to parse.</param>
+        /// <returns>The parsed details as a string.</returns>
+        private string ParseNDEF(ProximityMessage message) {
+            var output = "";
+
+            using (var buf = DataReader.FromBuffer(message.Data)) {
+                NdefRecordUtility.ReadNdefRecord(buf,recordList);
+
+                for (int i = 0, recordNumber = 0, spRecordNumber = 0; i < recordList.Count; i++)
+                {
+                    NdefRecord record = recordList.ElementAt(i);
+
+                    if (!record.IsSpRecord ) 
+                    {
+                        if (System.Text.Encoding.UTF8.GetString(record.Type, 0, record.TypeLength) == "Sp") 
+                        {
+                            //output = output + "\n --End of Record No." + recordNumber; spRecordNumber = 0;
+                            continue;
+                        }
+                        else 
+                        { 
+                            recordNumber++; 
+                            //output = output + "\n --Record No." + recordNumber; 
+                        }
+                    }
+                    else
+                    {
+                        if (spRecordNumber == 0) 
+                        { 
+                            recordNumber++; 
+                            //output = output + "\n --Record No." + recordNumber; 
+                        }
+
+                        spRecordNumber++;
+                        //output = output + "\n Sp sub-record No." + spRecordNumber; 
+                    }
+
+                    //output = output + "\n MB:" + ((record.Mb) ? "1;" : "0;");
+                    //output = output + " ME:" + ((record.Me) ? "1;" : "0;");
+                    //output = output + " CF:" + ((record.Cf) ? "1;" : "0;");
+                    //output = output + " SR:" + ((record.Sr) ? "1;" : "0;");
+                    //output = output + " IL:" + ((record.Il) ? "1;" : "0;");
+
+                    string typeName = NdefRecordUtility.GetTypeNameFormat(record);                     
+                    
+                    if (record.TypeLength > 0) 
+                    { 
+                        //output = output + "\n Type: " + typeName + ":"
+                          //  + System.Text.Encoding.UTF8.GetString(record.Type, 0, record.TypeLength); 
+                    }
+
+                    if ((record.Il) && (record.IdLength > 0))
+                    {
+                        output = output + "\n Id:"
+                            + System.Text.Encoding.UTF8.GetString(record.Id, 0, record.IdLength); 
+                    }
+
+                    if ((record.PayloadLength > 0) && (record.Payload != null))
+                    {
+                        if ((record.Tnf == 0x01)
+                            && (System.Text.Encoding.UTF8.GetString(record.Type, 0, record.TypeLength) == "U"))
+                        {
+                            NdefUriRtd uri = new NdefUriRtd();
+                            NdefRecordUtility.ReadUriRtd(record, uri);
+                            //output = output + "\n Uri: " + uri.GetFullUri();
+                        }
+                        else if ((record.Tnf == 0x01)
+                            && (System.Text.Encoding.UTF8.GetString(record.Type, 0, record.TypeLength) == "T"))
+                        {
+                            NdefTextRtd text = new NdefTextRtd();
+                            NdefRecordUtility.ReadTextRtd(record, text);
+                            //output = output + "\n Language: " + text.Language;
+                            //output = output + "\n Encoding: " + text.Encoding;
+                            output = output + "\n TagUID: " + text.Text;
+                        }
+                        else
+                        {
+                            if (record.Tnf==0x01) 
+                            {
+                               // output = output + "\n Payload:"
+                                 //   + System.Text.Encoding.UTF8.GetString(record.Payload, 0, record.Payload.Length);
+                            }
+                        }
+                    }
+
+                    if (!record.IsSpRecord) 
+                    { 
+                        //output = output + "\n --End of Record No." + recordNumber;
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Subscribes for NDEF messages. This ensures that we get notified
+        /// about the NFC events.
+        /// </summary>
+        private void SubscribeForMessage()
+        {            
+            if (device != null)
+            {
+                recordList.Clear();
+                subscriptionId = device.SubscribeForMessage("NDEF", MessageReceived);
+            }
+        }
+
+
+
+
+
+
+
+
+
 
         async private void Scan_Button_Clicked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
